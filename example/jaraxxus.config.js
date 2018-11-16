ca9bca131e8cc5098eba0701c68154582e0d2a59
Milk <luming@zhongan.com>
2018-11-16 18:04:18 +0800

    image

--- example/jaraxxus.config.js
+++ example/jaraxxus.config.js
@@ -1,15 +1,12 @@
 const dev = {
   assetsPublicPath: '/',
   assetsSubDirectory: 'static',
-  cssModules: false,
   devtool: 'eval-source-map',
   port: 3000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-  autoOpenBrowser: true,
   errorOverlay: true,
   notifyOnErrors: true,
   poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
   proxyTable: {},
-  contentBase: 'public',
   useEslint: false,
   showEslintErrorsInOverlay: true,
   before: function(app) {
@@ -25,6 +22,7 @@ module.exports = {
   entry: 'src/index.js',
   srcPath: 'src',
   appHtml: 'public/index.html',
+  cssModules: false,
   dev,
   build
 }
--- example/src/pages/PageAA/view.js
+++ example/src/pages/PageAA/view.js
@@ -6,6 +6,7 @@ export default class View extends Component {
     return (
       <div>
         <h1>This is PageAA</h1>
+        <img src={require('@/img/a.jpg')} style={{ width: '100%' }} />
       </div>
     )
   }
